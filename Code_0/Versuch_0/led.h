6894b48fdac50051baf5547cb654c0f80dc668bd
pouya_sh <pouya.esfahani@rwth-aachen.de>
2023-04-22 18:45:31 +0200

    Added Code_0 and moved the original files the Code_0 has the Implementations of Übungen buttonmodules and Anstreuerung des LED-Bargraphen both work fine and the led.h file is based on the setting in PC-POOL/ADA

---
+++ Code_0/Versuch_0/led.h
@@ -0,0 +1,24 @@
+/*
+ * led.h
+ *
+ * Created: 4/22/2023 5:35:36 PM
+ *  Author: Pooya
+ */ 
+
+
+#ifndef LED_H_
+#define LED_H_
+
+///----------------------------------------------------------------------------
+// Function headers
+//----------------------------------------------------------------------------
+
+//! initializes the led
+void led_init(void);
+
+//! the fun function from Versuch0
+void led_fun(void);
+
+
+
+#endif /* LED_H_ */
